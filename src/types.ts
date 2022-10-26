7d30b48877e0f380956241a9d47c29ccb0463dfc
qiushi <qiushi.yann@gmail.com>
2022-10-25 21:17:08 -0500

    add clipstash

--- src/types.ts
+++ src/types.ts
@@ -3,12 +3,12 @@ import type { MDXInstance } from "astro";
 export interface FrontMatter {
   title: string;
   date: string;
-  image: string;
   description: string;
   tags: string[];
+  link: string;
+  image?: string;
   lang?: string;
   group?: string;
-  link?: string;
 }
 
 export type Project = MDXInstance<FrontMatter>;
