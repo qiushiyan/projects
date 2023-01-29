f47a21752dd28013dff1d6c90b5ac532f1fe4909
Qiushi Yan <qiushi.yann@gmail.com>
2023-01-29 12:21:26 -0600

    make schema less strict

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,16 @@
-
-
 import { z, defineCollection } from 'astro:content'
+
+const randomImage = "https://source.unsplash.com/500x300?tech"
+
 const projectCollection = defineCollection({
     schema: z.object({
         title: z.string(),
-        image: z.string(),
+        image: z.string().default(randomImage),
         group: z.enum(['data-science', 'software', "other"]),
-        date: z.date().transform((str) => new Date(str)),
+        date: z.date().transform((str) => new Date(str)).default(new Date()),
         description: z.string(),
-        tags: z.array(z.string()),
-        link: z.string().url()
+        tags: z.array(z.string()).default([]),
+        link: z.string().url().optional()
     })
 })
 
