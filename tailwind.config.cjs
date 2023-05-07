0bb2d3450510bc42a372955e7a72ff02ff294362
Qiushi Yan <qiushi.yann@gmail.com>
2023-05-06 22:41:59 -0500

    tridata project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  mode: "jit",
-  content: ["./src/**/*.{astro, jsx, tsx, mdx}", "./public/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ["./src/**/*.{astro, jsx, tsx, mdx}", "./public/**/*.html"],
+	theme: {
+		extend: {
+			typography: ({ theme }) => ({
+				DEFAULT: {
+					css: {},
+				},
+			}),
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
 };
